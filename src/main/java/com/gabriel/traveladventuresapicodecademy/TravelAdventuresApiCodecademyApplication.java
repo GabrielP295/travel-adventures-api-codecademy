5472fc65975e4332098cf487a5eec15434a3690b
Gabriel Paniagua <gabrielpan295@gmail.com>
2026-05-27 13:26:01 -0500

    chore: initialize Spring Boot project

---
+++ src/main/java/com/gabriel/traveladventuresapicodecademy/TravelAdventuresApiCodecademyApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.traveladventuresapicodecademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAdventuresApiCodecademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAdventuresApiCodecademyApplication.class, args);
+    }
+
+}
