21e4e42e43e56fe90ab419f1de379c9ce928a37f
Gabriel Paniagua <gabrielpan295@gmail.com>
2026-05-27 22:42:03 -0500

    style(adventures-controller): organize imports to remove use of wildcards

--- src/main/java/com/gabriel/traveladventuresapicodecademy/controllers/TravelAdventuresController.java
+++ src/main/java/com/gabriel/traveladventuresapicodecademy/controllers/TravelAdventuresController.java
@@ -3,7 +3,16 @@ package com.gabriel.traveladventuresapicodecademy.controllers;
 import com.gabriel.traveladventuresapicodecademy.entities.Adventure;
 import com.gabriel.traveladventuresapicodecademy.repositories.AdventureRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -41,9 +50,8 @@ public class TravelAdventuresController {
     }
 
     @PutMapping("/{id}")
-    public Adventure updateAdventure(
-            @RequestBody Adventure adventure,
-            @PathVariable int id) {
+    public Adventure updateAdventure(@RequestBody Adventure adventure,
+                                     @PathVariable int id) {
         Optional<Adventure> adventureToUpdate = adventureRepository.findById(id);
         if (!adventureToUpdate.isPresent()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ID not found.");
